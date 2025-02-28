507b61cc73ef59362c49732a8685a6a08fedfd41
jou <jou.42.rio@gmail.com>
2025-02-28 14:29:08 -0300

    format instructions

--- module00/ex01/main.cpp
+++ module00/ex01/main.cpp
@@ -38,6 +38,9 @@ int main(void)
     std::string input;
     PhoneBook phonebook;
 
+    std::cout << std::endl << "add - add new contact" << std::endl;
+    std::cout << "search - display list of contacts" << std::endl;
+    std::cout << "exit - end program" << std::endl << std::endl;
     getline(std::cin, input);
     while (input != "exit")
     {
