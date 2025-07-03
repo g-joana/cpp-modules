cc5fcb7b771db38c97d3e9ebdabcb453300c3acd
jou <jou.42.rio@gmail.com>
2025-07-03 09:08:14 -0300

    ex00

--- module04/ex00/main.cpp
+++ module04/ex00/main.cpp
@@ -9,7 +9,6 @@
 int main() {
     // Cat tchego;
     // Dog *ozzy = new Dog;
-    // // Animal * ozzy = new Dog(); // why it doesnt work?
     //
     // std::cout << "tchego: ";
     // tchego.makeSound();
@@ -17,18 +16,17 @@ int main() {
     // ozzy->makeSound();
     // delete ozzy;
 
-    const WrongAnimal* meta = new WrongAnimal();
-    const WrongAnimal* i = new WrongCat();
-    // const Animal* meta = new Animal();
+    const WrongAnimal* wrong = new WrongAnimal();
+    // const WrongAnimal* i = new WrongCat();
     const Animal* j = new Dog();
-    // const Animal* i = new Cat();
+    const Animal* i = new Cat();
     std::cout << j->getType() << " " << std::endl;
     std::cout << i->getType() << " " << std::endl;
-    i->makeSound(); //will output the cat sound!
+    i->makeSound();
     j->makeSound();
-    meta->makeSound();
+    wrong->makeSound();
     delete i;
     delete j;
-    delete meta;
+    delete wrong;
     return 0;
 }
