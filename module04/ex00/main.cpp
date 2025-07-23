d3d23b6b7fdb02be16ca94281b6f980ce85d4f35
jou <jou.42.rio@gmail.com>
2025-07-23 19:23:44 -0300

    m04e00

--- module04/ex00/main.cpp
+++ module04/ex00/main.cpp
@@ -24,35 +24,38 @@ int main() {
 
     std::cout << std::endl;
     std::cout << "CONTRUCTORS" << std::endl;
-    std::cout << "> new wrong animal (wrongCat):" << std::endl;
+    std::cout << " > new wrong animal (wrongCat):" << std::endl;
     const WrongAnimal* wrongCat = new WrongCat();
-    std::cout << "> new cat (cat):" << std::endl;
+    std::cout << " > new cat (cat):" << std::endl;
     Animal* cat = new Cat();
-    std::cout << "> new copy cat (copy_cat):" << std::endl;
+    std::cout << " > new copy cat (copy_cat):" << std::endl;
     Animal* copyCat(cat);
-    std::cout << "> new cat (cat2):" << std::endl;
+    std::cout << " > new cat (cat2):" << std::endl;
     const Animal* cat2;
+
     std::cout << std::endl;
     std::cout << "MEMBER FUNCS" << std::endl;
-    std::cout << "> cat2 = cat :" << std::endl;
+    std::cout << " > cat2 = cat :" << std::endl;
     cat2 = cat;
-    std::cout << "> type of cat :" << std::endl;
-    std::cout << cat->getType() << std::endl;
+    std::cout << " > type of cat2 :" << std::endl;
+    std::cout << cat2->getType() << std::endl;
+
     std::cout << std::endl;
     std::cout << "MAKE SOUND" << std::endl;
-    std::cout << "> cat says :" << std::endl;
+    std::cout << " > cat says :" << std::endl;
     cat->makeSound();
-    std::cout << "> copyCat says :" << std::endl;
+    std::cout << " > copyCat says :" << std::endl;
     copyCat->makeSound();
-    std::cout << "> cat2 says :" << std::endl;
+    std::cout << " > cat2 says :" << std::endl;
     cat2->makeSound();
-    std::cout << "> wrong says :" << std::endl;
+    std::cout << " > wrong says :" << std::endl;
     wrongCat->makeSound();
+
     std::cout << std::endl;
     std::cout << "DELETE" << std::endl;
-    std::cout << "> delete cat :" << std::endl;
+    std::cout << " > delete cat :" << std::endl;
     delete cat;
-    std::cout << "> delete wrongCat :" << std::endl;
+    std::cout << " > delete wrongCat :" << std::endl;
     // deletes wrong animal, does not delete wrong cat;
     delete wrongCat;
     return 0;
