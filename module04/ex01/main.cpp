c293fe14fbcc0bf745dd06cb5f8cca09dfe13859
jou <jou.42.rio@gmail.com>
2025-07-23 19:34:13 -0300

    m04e01

--- module04/ex01/main.cpp
+++ module04/ex01/main.cpp
@@ -2,14 +2,17 @@
 // #include "WrongCat.hpp"
 #include "Cat.hpp"
 #include "Dog.hpp"
+#include <iostream>
 
 int main() {
     int i = 0;
     Animal *animals[10];
-    Cat gato;
-    gato.setIdea("miau?");
-    gato.printIdeas();
+    // Cat gato;
+    // gato.setIdea("miau?");
+    // gato.printIdeas();
 
+    std::cout << std::endl;
+    std::cout << "CONTRUCTORS" << std::endl;
     while (i < 10)
     {
         if (i >= 5)
@@ -18,6 +21,8 @@ int main() {
             animals[i] = new Dog();
         i++;
     }
+    std::cout << std::endl;
+    std::cout << "MAKE SOUND" << std::endl;
     i = 0;
     while (i < 10)
     {
@@ -29,6 +34,8 @@ int main() {
         animals[i]->makeSound();
         i++;
     }
+    std::cout << std::endl;
+    std::cout << "DELETE" << std::endl;
     i = 0;
     while (i < 10)
     {
